0feca267f1453a98cfd1a97f93ea7cf8874f83ca
Luis Carlos Rangel <luiscrangel19@gmail.com>
2023-03-28 18:13:51 -0500

    build navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
-
 function App() {
 
 
   return (
     <div className="App">  
-    <div className='sidebar'>sidebar</div>
-    <div className='content'>content</div>
+    <div className='sidebar'>
+    <h1>LOGO</h1>
+    <ul className='sidebar-list'>
+      <li><i className='bx bx-home-alt'></i>Home</li>
+      <li><i className='bx bx-envelope'></i>Messages</li>
+      <li><i className='bx bx-line-chart' ></i>Statistics</li>
+      <li><i className='bx bx-dice-5'></i>Products</li>
+      <li><i className='bx bx-slider'></i>Settings</li>      
+    </ul>
+    <p><i className='bx bx-log-in' ></i>LOGOUT</p>
+    </div>
+    <div className='content'>     
+    <div className='card-content'>
+      <span></span>
+    </div>
+  
+    </div>
      
     </div>
   )
